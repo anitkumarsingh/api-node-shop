aa132277519861becbf68341e5871b89d5d17861
Anit Kumar <anikumarsingh99@gmail.com>
2019-09-29 15:47:10 +0530

    error handing

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const express = require('express');
-const app = express();
+const morgan = require('morgan');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+// Middlewares
+const app = express();
+app.use(morgan('dev'));
+
+// Routes
 app.use('/products',productRoutes);
 app.use('/orders',orderRoutes);
 
+// Error handing
+app.use((req,res,next)=>{
+    const error = new Error('Not Found!');
+    error.status = 404;
+    next(error);
+})
+app.use((error,req,res,next)=>{
+    res.status(error.status || 500);
+    res.json({
+        error:{
+            message:error.message
+        }
+    })
+})
 module.exports = app;
